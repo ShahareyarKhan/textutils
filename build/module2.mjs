f241f74ee600a15768d502b4eaa352004c9dfa67
Md Shahareyar Anjum Khan <124686509+ShahareyarKhan@users.noreply.github.com>
2023-06-05 00:15:04 +0530

    Add files via upload

---
+++ build/module2.mjs
@@ -0,0 +1,7 @@
+import x,{a,b,c} from './module1.mjs'
+console.log(x);
+console.log(a);
+console.log(b);
+console.log(c);
+// if default export call by any variable
+// if only export call by same name
